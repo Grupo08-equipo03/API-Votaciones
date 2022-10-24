26b93cdaadeb3b076f11820c40a389aa8893e3b6
Camilo Castellar <camilocastelar3@gmail.com>
2022-10-24 17:15:50 -0500

    Creacion de urls para Votos (Camilo)

--- main.py
+++ main.py
@@ -8,6 +8,16 @@ from waitress import serve
 app=Flask(__name__)
 cors = CORS(app)
 
+from Controladores.ControladorCandidato import ControladorCandidato
+from Controladores.ControladorPartido import ControladorPartido
+from Controladores.ControladorMesa import ControladorMesa
+from Controladores.ControladorVoto import ControladorVoto
+
+miControladorCandidatos = ControladorCandidato()
+miControladorPartidos = ControladorPartido()
+miControladorMesas = ControladorMesa()
+miControladorVotos = ControladorVoto()
+
 # ---------------------------------------------------------- #
 @app.route("/",methods=['GET'])
 def test():
@@ -22,7 +32,39 @@ def test():
 
 
 
+
+
+
 # ---------------------------------------------------------- #
+# URLS de Camilo
+@app.route("/votos", methods=['GET'])
+def getVotos():
+    json = miControladorVotos.index()
+    return jsonify(json)
+
+@app.route("/votos/<string:id>", methods=['GET'])
+def getVoto(id):
+    json = miControladorVotos.show(id)
+    return jsonify(json)
+
+@app.route("/votos/candidatos/<string:id_candidato>/mesas/<string:id_mesa>", methods=['POST'])
+def crearVoto(id_candidato, id_mesa):
+    data = request.get_json()
+    json = miControladorVotos.create(data, id_candidato, id_mesa)
+    return jsonify(json)
+
+@app.route("/votos/<string:id_voto>/candidatos/<string:id_candidato>/mesas/<string:id_mesa>", methods=['PUT'])
+def modificarVoto(id_voto, id_candidato, id_mesa):
+    data = request.get_json()
+    json = miControladorVotos.update(id_voto, data, id_candidato, id_mesa)
+    return jsonify(json)
+
+@app.route("/votos/<string:id_voto>", methods=['DELETE'])
+def eliminarInscripcion(id_voto):
+    json=miControladorVotos.delete(id_voto)
+    return jsonify(json)
+# ---------------------------------------------------------- #
+
 def loadFileConfig():
     with open('configuracion.json') as f:
         data = json.load(f)
@@ -32,4 +74,5 @@ if __name__=='__main__':
     dataConfig = loadFileConfig()
     print("Server running : "+"http://"+dataConfig["url-backend"]+":" + str(dataConfig["port"]))
     serve(app,host=dataConfig["url-backend"],port=dataConfig["port"])
+
 # ---------------------------------------------------------- #
