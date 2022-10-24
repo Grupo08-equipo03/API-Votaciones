7a099b8f1bf80549abd229d041e00fe736d62ede
Sayala26 <sayala79@unisalle.edu.co>
2022-10-24 18:27:21 -0500

    crud partidos

--- main.py
+++ main.py
@@ -25,7 +25,28 @@ def test():
     json["message"]="Server running ..."
     return jsonify(json)
 # ---------------------------------------------------------- #
-
+@app.route("/partidos",methods=['GET'])
+def getPartidos():
+    json=miControladorPartido.index()
+    return jsonify(json)
+@app.route("/partidos/<string:id>",methods=['GET'])
+def getPartido(id):
+    json=miControladorPartido.show(id)
+    return jsonify(json)
+@app.route("/partidos",methods=['POST'])
+def crearPartido():
+    data = request.get_json()
+    json=miControladorPartido.create(data)
+    return jsonify(json)
+@app.route("/partidos/<string:id>",methods=['PUT'])
+def modificarPartido(id):
+    data = request.get_json()
+    json=miControladorPartido.update(id,data)
+    return jsonify(json)
+@app.route("/partidos/<string:id>",methods=['DELETE'])
+def eliminarPartido(id):
+    json=miControladorPartido.delete(id)
+    return jsonify(json)
 
 
 
